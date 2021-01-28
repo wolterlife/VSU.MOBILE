65649b5168bbe9af95559f22726a6ea087eb2006
wolterlife <wolter.important@gmail.com>
2021-01-28 23:56:16 +0300

    Test commit

--- app/src/main/java/com/vsu/pocket/ui/link/LinkFragment.kt
+++ app/src/main/java/com/vsu/pocket/ui/link/LinkFragment.kt
@@ -15,61 +15,35 @@ import com.vsu.pocket.R
 import kotlinx.android.synthetic.main.fragment_link.*
 
 
-class LinkFragment : Fragment() {
 
-   // private lateinit var linkViewModel: LinkViewModel // DELL NZ ZACH
+
+class LinkFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
     {
-        val vsuintent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+        println(" ")
+        println("|||||| PERPENDIKULAR ||||||| ")
+        /* val vsuintent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
         val sdointent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
         val youtubeintent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
         val instagramintent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
         val vkintent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
         val facebookintent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
         val twitterintent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
-        val telegramintent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
-
-       // linkViewModel = ViewModelProviders.of(this).get(LinkViewModel::class.java) // DELL NZ ZACH
+        val telegramintent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));*/
         val root = inflater.inflate(R.layout.fragment_link, container, false)
+        ////////////////////
 
-        vsubutton.setOnClickListener {
-            startActivity(vsuintent);
-        }
-        youtubebutton.setOnClickListener {
-            startActivity(youtubeintent);
-        }
-        sdobutton.setOnClickListener {
-            startActivity(sdointent);
-        }
-        instagrambutton.setOnClickListener {
-            startActivity(instagramintent);
-        }
-        vkbutton.setOnClickListener {
-            startActivity(vkintent);
-        }
-        facebookbutton.setOnClickListener {
-            startActivity(facebookintent);
-        }
-        twitterbutton.setOnClickListener {
-            startActivity(twitterintent);
-        }
-        telegrambutton.setOnClickListener {
-            startActivity(telegramintent);
-        }
-
+        ////////////////////
         return root
+
     }
+
+
+
 }
 
 
 
-/*fun openNewTabWindow(urls: String, context: Context, view: View) {
-          val uris = Uri.parse(urls)
-          val intents = Intent(Intent.ACTION_VIEW, uris)
-          val b = Bundle()
-          b.putBoolean("new_window", true)
-          intents.putExtras(b)
-          context.startActivity(intents)
-          }*/
+
 
